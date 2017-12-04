f7232e262b05f9f2f4e9307c0a995fca6bb948fd
mcagalj <mario.cagalj@fesb.hr>
2017-12-04 12:36:28 +0100

    Updating...

--- bits-of-react/web-page/src/index.js
+++ bits-of-react/web-page/src/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-import React,  { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React,  { Component } from 'react'
+import ReactDOM from 'react-dom'
 import classnames from 'classnames'
 import 'styles/main.css'
 
